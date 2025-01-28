3213b6880bf04415b7610320d119e5fb0ea2ffde
BORREANI Théo <theoborreani.toba@gmail.com>
2025-01-28 10:58:16 +0100

    ajout de la création duu nouveau chat

--- src/app/pages/chatbot/chatbot.component.ts
+++ src/app/pages/chatbot/chatbot.component.ts
@@ -94,4 +94,12 @@ export class ChatbotComponent {
     this.httpService.changeSession(session);
     this.messages = this.httpService.getMessages();
   }
+
+  public ajouterNewChat(): void {
+    this.httpService.addNewChat();
+    this.messages = this.httpService.getMessages();
+    this.httpService.getSessions().then(sessions => {
+      this.historique_title = sessions;
+    });
+  }
 }
